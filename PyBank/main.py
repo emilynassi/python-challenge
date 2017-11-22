1913c00f187f857910aad21ac429801d82366ead
Emily Nassi <emilynassi@Emilys-MacBook-Pro-2.local>
2017-11-21 19:07:16 -0500

    Updated main file and added csvs

---
+++ PyBank/main.py
@@ -0,0 +1,6 @@
+import os
+import csv
+
+budgetdata1_csv = os.path.join("..", "Documents", "Class", "HW", "python-challenge", "PyBank", "budget_data_1.csv")
+budgetdata2_csv = os.path.join("..", "Documents", "Class", "HW", "python-challenge", "PyBank", "budget_data_2.csv")
+
