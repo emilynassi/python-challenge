785a9399c31f1efb38cd806762c69733893510df
Emily Nassi <emilynassi@Emilys-MBP-2.fios-router.home>
2017-11-26 13:39:48 -0500

    Completed PyParagraph with text stored in script

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -3,13 +3,16 @@ passage = "The Mirror will be moved to a new home tomorrow, Harry, and I ask you
 #find approximate word count
 word_count = len(passage.split())
  
- #find aproximate word count
-sentence_count = len(passage.split("."))
+ #find aproximate sentence count
+sentence_count = passage.count(".")
 
 #split up words by spaces
 words = passage.split()
 #count characters in each word, add together and divide by amount of all words
 character_count = sum(len(word) for word in words) / len(words)
+
+#split up sentences by periods
+average_word_per_sentence = word_count / sentence_count
 
 
 print("Paragraph Analysis")
@@ -17,3 +20,4 @@ print("-----------------------------------")
 print("Approximate Word Count:",str(word_count))
 print("Approximate Sentence Count:",str(sentence_count))
 print("Average Letter Count:",str(character_count))
+print("Average Sentence Count:",str(average_word_per_sentence))
