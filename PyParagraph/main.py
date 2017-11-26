a0e8903543fa74462cdbe1332f7457b30172110f
Emily Nassi <emilynassi@Emilys-MBP-2.fios-router.home>
2017-11-26 12:23:12 -0500

    PyParagraph Updates

---
+++ PyParagraph/main.py
@@ -0,0 +1,10 @@
+s = "The Mirror will be moved to a new home tomorrow, Harry, and I ask you not to go looking for it again. If you ever do run across it, you will now be prepared. It does not do to dwell on dreams and forget to live, remember that. Now, why don't you put that admirable cloak back on and get off to bed."
+
+word_count = len(s.split())
+sentence_count = len(s.split("."))
+
+
+print("Paragraph Analysis")
+print("-----------------------------------")
+print("Approximate Word Count:",str(word_count))
+print("Approximate Sentence Count:",str(sentence_count))
